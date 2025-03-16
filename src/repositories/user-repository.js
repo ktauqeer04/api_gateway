140ea6659eb28720fc1192e33081d4a4bf963eca
Mohammad Tauqeer Kondkari <mohammedtauqeer4oct@gmail.com>
2025-03-16 23:50:30 +0530

    added user

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,13 @@
+const crudRespository = require("./crud-repository");
+const { User } = require('../models');
+
+
+class UserRepository extends crudRespository{
+    constructor(){
+        super(City);
+    }
+
+    //
+}
+
+module.exports = CityRepository;
